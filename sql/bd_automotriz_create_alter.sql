b19464b7823adf228236c45fb010f5c65e1e4061
Rodrigo Vargas <rodrigovargassanhueza2@gmail.com>
2019-05-03 22:11:11 -0400

    mejoras en modelo automotriz

--- sql/bd_automotriz_create_alter.sql
+++ sql/bd_automotriz_create_alter.sql
@@ -1,6 +1,5 @@
 CREATE TABLE T_MARCA(
-	id_marca				int not null,
-	id_modelo				int not null,
+	id_marca				int not null AUTO_INCREMENT,
 	nombre_marca 			varchar(100) null,
 	descripcion_marca 		varchar(100) null,
 	fecha_creacion 			date null,
@@ -12,7 +11,8 @@ CREATE TABLE T_MARCA(
 )ENGINE=InnoDb;
 
 CREATE TABLE T_MODELO(
-	id_modelo				int not null,
+	id_modelo				int not null AUTO_INCREMENT,
+    id_marca				int null,
 	nombre_modelo			varchar(100) null,
 	descripcion_modelo		varchar(100) null,
 	fecha_creacion 			date null,
@@ -29,26 +29,24 @@ CREATE TABLE T_REGISTROS_MARCA_CONCESIONARIA(
 )ENGINE=InnoDb;
 
 CREATE TABLE T_CONCESIONARIA(
-	id_concesionaria		int not null,
-	id_sucursal 			int not null,
-	nombre_sucursal			varchar(100) null,
-	descripcion_sucursal 	varchar(100) null,
-	id_comuna				int null,
-	direccion				varchar(100),
-	fecha_creacion 			date null,
-  	fecha_modificacion 		date null,
-  	usuario_creacion 		int null,
-  	usuario_modificacion 	int null,
- 	vigente 				bit,
+	id_concesionaria			int not null AUTO_INCREMENT,
+	nombre_concesionaria		varchar(100) null,
+	descripcion_concesionaria 	varchar(100) null,
+	fecha_creacion 				date null,
+  	fecha_modificacion 			date null,
+  	usuario_creacion 			int null,
+  	usuario_modificacion 		int null,
+ 	vigente 					bit,
  	CONSTRAINT PK_T_CONCESIONARIA PRIMARY KEY (id_concesionaria)
 )ENGINE=InnoDb;
 
 CREATE TABLE T_SUCURSAL(
-	id_sucursal 			int not null,
+	id_sucursal 			int not null AUTO_INCREMENT,
 	nombre_sucursal			varchar(100) null,
 	descripcion_sucursal 	varchar(100) null,
 	id_comuna				int null,
-	direccion				varchar(100),
+	id_concesionaria		int null,
+	direccion_sucursal		varchar(100),
 	fecha_creacion 			date null,
   	fecha_modificacion 		date null,
   	usuario_creacion 		int null,
@@ -58,14 +56,14 @@ CREATE TABLE T_SUCURSAL(
 )ENGINE=InnoDb;
 
 /* === FOREIGN === */
-ALTER TABLE T_MARCA ADD CONSTRAINT FK_T_MARCA_MODELO FOREIGN KEY(id_modelo)
-REFERENCES T_MODELO (id_modelo);
+ALTER TABLE T_MODELO ADD CONSTRAINT FK_T_MODELO_MARCA FOREIGN KEY(id_marca)
+REFERENCES T_MARCA (id_marca);
 
-ALTER TABLE T_CONCESIONARIA ADD CONSTRAINT FK_T_CONCESIONARIA_SUCURSAL FOREIGN KEY(id_sucursal)
-REFERENCES T_SUCURSAL (id_sucursal);
+ALTER TABLE T_SUCURSAL ADD CONSTRAINT FK_T_SUCURSAL_CONCESIONARIA FOREIGN KEY(id_concesionaria)
+REFERENCES T_CONCESIONARIA (id_concesionaria);
 
 ALTER TABLE T_REGISTROS_MARCA_CONCESIONARIA ADD CONSTRAINT FK_T_REGISTROS_MARCA_CONCESIONARIA_MARCA FOREIGN KEY(id_marca)
 REFERENCES T_MARCA (id_marca);
 
 ALTER TABLE T_REGISTROS_MARCA_CONCESIONARIA ADD CONSTRAINT FK_T_REGISTROS_MARCA_CONCESIONARIA_CONCESIONARIA FOREIGN KEY(id_concesionaria)
-REFERENCES T_CONCESIONARIA (id_concesionaria);
+REFERENCES T_CONCESIONARIA(id_concesionaria)
