09569bb7cd2ebe3b0b49c7a19c220f4f8e6fe31d
Rodrigo Vargas <rvargas@cooperativa.cl>
2019-05-15 18:49:09 -0400

    agrego validacion de rut y elimino archivos de bd_automotriz

--- src/validators/formularios/insert.js
+++ src/validators/formularios/insert.js
@@ -5,35 +5,36 @@ const rutValidacion = require('./../rutExtend');
 
 module.exports = joi.object().keys({
 
-    persona: {
-            id_usuario:joi.number().integer().required(),
-            rut_persona:rutValidacion.RutValidacion().RutValido().default(null),
-            nombre_completo_persona:joi.string().default(null),
-            correo_persona:joi.string().email().default(null),
-            telefono_persona:joi.string().default(null),
-    },
-    direccion: { 
-            id_comuna: joi.number().integer().default(347).required(),
-			calle_direccion: joi.string().allow(null),
-			numero_direccion: joi.number().integer().allow(null),
-			observacion_direccion: joi.string().allow(null),
-            cod_postal: joi.number().integer(),
+data:{ 
+        persona: {
+                id_usuario:joi.number().integer().required(),
+                rut_persona:rutValidacion.RutValidacion().RutValido().default(null).allow(null),
+                nombre_completo_persona:joi.string().default(null).allow(null),
+                correo_persona:joi.string().email().default(null).allow(null),
+                telefono_persona:joi.string().default(null).allow(null),
         },
-    formulario: { 
-            id_tipo_formulario: joi.number().integer().required(),
-		    nombre_formulario: joi.string().allow(null),
-		    descripcion_formulario: joi.string().allow(null),
-		    nombre_sitio_web: joi.string().allow(null),
-		    nuevo_valor_vehiculo: joi.number().integer().allow(null),
-		    antiguo_valor_vehiculo: joi.number().integer().allow(null),
-		    valor_bono_vehiculo: joi.number().integer().allow(null),
-		    valor_pie_vehiculo: joi.number().integer().allow(null),
-		    valor_cuota: joi.number().integer().allow(null),
-		    valor_matricula: joi.number().integer().allow(null),
-		    id_marca_vehiculo: joi.number().integer().allow(null),
-		    id_modelo_vehiculo: joi.number().integer().allow(null),
-		    id_concesionaria_vehiculo: joi.number().integer().allow(null),
-            id_sucursal_vehiculo: joi.number().integer().allow(null)
+        direccion: { 
+                id_comuna: joi.number().integer().default(347).required(),
+                calle_direccion: joi.string().allow(null).default(null),
+                numero_direccion: joi.number().integer().allow(null).default(null),
+                observacion_direccion: joi.string().allow(null).default(null),
+                cod_postal: joi.number().integer().allow(null).default(null),
+            },
+        formulario: { 
+                id_tipo_formulario: joi.number().integer().required(),
+                nombre_formulario: joi.string().allow(null).default(null),
+                descripcion_formulario: joi.string().allow(null).default(null),
+                nombre_sitio_web: joi.string().allow(null).default(null),
+                nuevo_valor_vehiculo: joi.number().integer().allow(null).default(null),
+                antiguo_valor_vehiculo: joi.number().integer().allow(null).default(null),
+                valor_bono_vehiculo: joi.number().integer().allow(null).default(null),
+                valor_pie_vehiculo: joi.number().integer().allow(null).default(null),
+                valor_cuota: joi.number().integer().allow(null).default(null),
+                valor_matricula: joi.number().integer().allow(null).default(null),
+                id_marca_vehiculo: joi.number().integer().allow(null).default(null),
+                id_modelo_vehiculo: joi.number().integer().allow(null),
+                id_concesionaria_vehiculo: joi.number().integer().allow(null).default(null),
+                id_sucursal_vehiculo: joi.number().integer().allow(null).default(null)
+        }
     }
-    
 });
